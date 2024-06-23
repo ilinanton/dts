01f8bca1348792381016e189ee56b102c92a7cc5
Anton Ilin <ilin.antonio@gmail.com>
2024-06-23 20:18:59 +0500

    refactor: move class

--- app/src/Domain/GitLab/Member/ValueObject/MemberWebUrl.php
+++ app/src/Domain/GitLab/Member/ValueObject/MemberWebUrl.php
@@ -2,7 +2,7 @@
 
 namespace App\Domain\GitLab\Member\ValueObject;
 
-use App\Domain\GitLab\Common\AbstractWebUrl;
+use App\Domain\GitLab\Common\ValueObject\AbstractWebUrl;
 
 final class MemberWebUrl extends AbstractWebUrl
 {
--- app/src/Domain/GitLab/Project/ValueObject/ProjectWebUrl.php
+++ app/src/Domain/GitLab/Project/ValueObject/ProjectWebUrl.php
@@ -2,7 +2,7 @@
 
 namespace App\Domain\GitLab\Project\ValueObject;
 
-use App\Domain\GitLab\Common\AbstractWebUrl;
+use App\Domain\GitLab\Common\ValueObject\AbstractWebUrl;
 
 final class ProjectWebUrl extends AbstractWebUrl
 {
