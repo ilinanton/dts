bb6a03afa69273a5e4ffdadadb69107a0e3f4751
Anton Ilin <ilin.antonio@gmail.com>
2024-06-23 20:12:07 +0500

    refactor: Add AbstractWebUrl

---
+++ app/src/Domain/GitLab/MergeRequest/ValueObject/MergeRequestWebUrl.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Domain\GitLab\MergeRequest\ValueObject;
+
+use App\Domain\GitLab\Common\AbstractWebUrl;
+
+final class MergeRequestWebUrl extends AbstractWebUrl
+{
+}
--- app/src/Domain/GitLab/Project/ValueObject/ProjectWebUrl.php
+++ app/src/Domain/GitLab/Project/ValueObject/ProjectWebUrl.php
@@ -2,27 +2,8 @@
 
 namespace App\Domain\GitLab\Project\ValueObject;
 
-use InvalidArgumentException;
+use App\Domain\GitLab\Common\AbstractWebUrl;
 
-final class ProjectWebUrl
+final class ProjectWebUrl extends AbstractWebUrl
 {
-    private string $value;
-
-    public function __construct(string $value)
-    {
-        $this->assertValueIsValid($value);
-        $this->value = $value;
-    }
-
-    public function getValue(): string
-    {
-        return $this->value;
-    }
-
-    private function assertValueIsValid(string $value): void
-    {
-        if (false === filter_var($value, FILTER_VALIDATE_URL, FILTER_FLAG_PATH_REQUIRED)) {
-            throw new InvalidArgumentException('Web url is incorrect!');
-        }
-    }
 }
