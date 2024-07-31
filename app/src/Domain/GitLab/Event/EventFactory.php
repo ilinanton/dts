e797a7ba6f13ff4b627cacc70848fa7c18ef8a08
Anton Ilin <ilin.antonio@gmail.com>
2024-07-31 17:56:51 +0500

    feat: add EventFactory

---
+++ app/src/Domain/GitLab/Event/EventFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Domain\GitLab\Event;
+
+
+use App\Domain\GitLab\Event\ValueObject\EventActionName;
+use App\Domain\GitLab\Event\ValueObject\EventAuthorId;
+use App\Domain\GitLab\Event\ValueObject\EventCreateAt;
+use App\Domain\GitLab\Event\ValueObject\EventId;
+use App\Domain\GitLab\Event\ValueObject\EventProjectId;
+use App\Domain\GitLab\Event\ValueObject\EventTargetId;
+use App\Domain\GitLab\Event\ValueObject\EventTargetIid;
+use App\Domain\GitLab\Event\ValueObject\EventTargetTitle;
+
+final class EventFactory
+{
+    public function create(array $data): Event
+    {
+        return new Event(
+            new EventId($data['id'] ?? 0),
+            new EventProjectId($data['project_id'] ?? 0),
+            new EventActionName($data['action_name'] ?? ''),
+            new EventTargetId($data['target_id'] ?? 0),
+            new EventTargetIid($data['target_iid'] ?? 0),
+            new EventAuthorId($data['author_id'] ?? 0),
+            new EventTargetTitle($data['target_title'] ?? ''),
+            new EventCreateAt($data['created_at'] ?? '')
+        );
+    }
+}
