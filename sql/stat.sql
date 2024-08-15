5df46f3136f4ccb213b0f547b1eab66b29324c7f
Anton Ilin <ilin.antonio@gmail.com>
2024-08-15 17:29:50 +0500

    feat: add sql to count direct commits

--- sql/stat.sql
+++ sql/stat.sql
@@ -1,10 +1,12 @@
 SELECT id, name, mr_created, mr_merged, mr_merged_without_approv, mr_approved, mr_self_approved,
+       committed_to_default_branch,
        (
            mr_created * mr_created_point +
            mr_merged * mr_merged_point +
            mr_merged_without_approv * mr_merged_without_approv_point +
            mr_approved * mr_approved_point +
-           mr_self_approved * mr_self_approved_point
+           mr_self_approved * mr_self_approved_point +
+           committed_to_default_branch * committed_to_default_branch_point
            ) AS score, after_at
 
 FROM (
@@ -53,13 +55,24 @@ FROM (
                 (
                     SELECT COUNT(*)
                     FROM git_lab_event e
-                             INNER JOIN git_lab_merge_request mr
-                                        ON mr.author_id = e.author_id
-                                            AND mr.id = e.target_id
+                    INNER JOIN git_lab_merge_request mr
+                            ON mr.author_id = e.author_id
+                           AND mr.id = e.target_id
                     WHERE e.author_id = u.id
                       AND e.created_at > d.date
                       AND e.action_name = 'approved'
-                ) AS mr_self_approved, 0 AS mr_self_approved_point
+                ) AS mr_self_approved, 0 AS mr_self_approved_point,
+                (
+                    SELECT COUNT(*)
+                    FROM git_lab_event e
+                    INNER JOIN git_lab_project p ON p.id = e.project_id AND p.default_branch = e.push_data_ref
+                    WHERE e.author_id = u.id
+                      AND e.created_at > d.date
+                      AND e.action_name = 'pushed to'
+                      AND e.push_data_ref_type = 'branch'
+                      AND e.push_data_commit_title NOT LIKE 'Merge branch%'
+                ) AS committed_to_default_branch, 0 AS committed_to_default_branch_point
+
          FROM git_lab_user u
                   LEFT JOIN (
              #            SELECT DATE_FORMAT(NOW() - INTERVAL 6 DAY, '%Y.%m.%d') AS date
@@ -73,5 +86,16 @@ FROM (
 ORDER BY score DESC
     # ORDER BY mr_approved DESC
     # ORDER BY mr_merged_without_approv DESC
+;
 
+SELECT COUNT(*) AS cnt, e.author_id, u.name
+FROM git_lab_event e
+INNER JOIN git_lab_user u ON u.id = e.author_id
+INNER JOIN git_lab_project p ON p.id = e.project_id AND p.default_branch = e.push_data_ref
+WHERE 1 = 1
+  AND e.action_name = 'pushed to'
+  AND e.push_data_ref_type = 'branch'
+  AND e.push_data_commit_title NOT LIKE 'Merge branch%'
+GROUP BY e.author_id
+ORDER BY cnt DESC
 ;
