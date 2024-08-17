36c118599bf69c39c1b142496ddefb79a603b3fb
Anton Ilin <ilin.antonio@gmail.com>
2024-08-18 01:03:03 +0500

    feat: add Stats

---
+++ app/src/Domain/GitLab/Stats/ValueObject/StatsTotal.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Domain\GitLab\Stats\ValueObject;
+
+use App\Domain\GitLab\Common\ValueObject\AbstractRequiredUnsignedInt;
+
+final readonly class StatsTotal extends AbstractRequiredUnsignedInt
+{
+}
