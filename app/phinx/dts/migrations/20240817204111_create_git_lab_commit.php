af18e581c447942cb42f921a83dcdbc33030c8d3
Anton Ilin <ilin.antonio@gmail.com>
2024-08-18 02:32:47 +0500

    fix: change length

--- app/phinx/dts/migrations/20240817204111_create_git_lab_commit.php
+++ app/phinx/dts/migrations/20240817204111_create_git_lab_commit.php
@@ -20,7 +20,7 @@ final class CreateGitLabCommit extends AbstractMigration
             ->addColumn('id', 'string', ['length' => 128, 'null' => false])
             ->addColumn('short_id', 'string', ['length' => 32, 'null' => false])
             ->addColumn('title', 'string', ['length' => 255, 'null' => true])
-            ->addColumn('message', 'string', ['length' => 1024, 'null' => true])
+            ->addColumn('message', 'string', ['length' => 4000, 'null' => true])
             ->addColumn('created_at', 'datetime', ['null' => false])
             ->addColumn('web_url', 'string', ['length' => 255, 'null' => false])
 
