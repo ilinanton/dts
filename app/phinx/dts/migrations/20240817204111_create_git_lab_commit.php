bae8dfe7d412ab069960edfbeebf7fb61bafb5ce
Anton Ilin <ilin.antonio@gmail.com>
2024-08-18 21:13:20 +0500

    fix: increase length for commit title

--- app/phinx/dts/migrations/20240817204111_create_git_lab_commit.php
+++ app/phinx/dts/migrations/20240817204111_create_git_lab_commit.php
@@ -19,7 +19,7 @@ final class CreateGitLabCommit extends AbstractMigration
         )
             ->addColumn('id', 'string', ['length' => 128, 'null' => false])
             ->addColumn('short_id', 'string', ['length' => 32, 'null' => false])
-            ->addColumn('title', 'string', ['length' => 255, 'null' => true])
+            ->addColumn('title', 'string', ['length' => 1024, 'null' => true])
             ->addColumn('created_at', 'datetime', ['null' => false])
             ->addColumn('web_url', 'string', ['length' => 255, 'null' => false])
 
