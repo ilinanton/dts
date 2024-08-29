183fdf6677aeb544da3a23ef8cb42cd2af3218ee
Anton Ilin <ilin.antonio@gmail.com>
2024-08-29 19:33:48 +0500

    feat: test code

--- app/src/Application/Git/SyncGitDataUseCase.php
+++ app/src/Application/Git/SyncGitDataUseCase.php
@@ -17,16 +17,23 @@ final readonly class SyncGitDataUseCase implements UseCaseInterface
     {
         $result = shell_exec("cd ./projects && ls -d */");
         $projects = explode(PHP_EOL, $result);
-        foreach ($projects as $item) {
-            if (0 === strlen($item)) {
+        foreach ($projects as $project) {
+            if (0 === strlen($project)) {
                 continue;
             }
 
-            $dir = "./projects/{$item}";
+            $dir = "./projects/{$project}";
 
             $result = shell_exec("cd {$dir} && git log --format='%aE|%cE|%aN|%cN' --since='{$this->syncDateAfter}' | sort -u ");
             $users = explode(PHP_EOL, $result);
             var_dump($users);
+
+            $result = shell_exec("cd {$dir} && git log --shortstat --no-merges --format='{|c|}commit: %H{|p|}date: %aD{|p|}email: %aE{|p|}stat:' --since='{$this->syncDateAfter}' | tr '\n' ' '");
+            $commits = explode('{|c|}', $result);
+            foreach ($commits as $commit) {
+                $commit = explode('{|p|}', $commit);
+                var_dump($commit);
+            }
         }
     }
 }
