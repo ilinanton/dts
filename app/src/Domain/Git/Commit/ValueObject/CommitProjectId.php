7994bcca95b08711143f8197f08f6a26cbc2d9f7
Anton Ilin <ilin.antonio@gmail.com>
2024-08-29 23:57:59 +0500

    feat: add ids

--- app/src/Domain/Git/Commit/ValueObject/CommitProjectId.php
+++ app/src/Domain/Git/Commit/ValueObject/CommitProjectId.php
@@ -4,6 +4,6 @@ namespace App\Domain\Git\Commit\ValueObject;
 
 use App\Domain\Common\ValueObject\AbstractRequiredUnsignedInt;
 
-final readonly class ProjectId extends AbstractRequiredUnsignedInt
+final readonly class CommitProjectId extends AbstractRequiredUnsignedInt
 {
 }
--- app/src/Domain/Git/User/User.php
+++ app/src/Domain/Git/User/User.php
@@ -3,23 +3,17 @@
 namespace App\Domain\Git\User;
 
 use App\Domain\Git\User\ValueObject\UserEmail;
+use App\Domain\Git\User\ValueObject\UserId;
 use App\Domain\Git\User\ValueObject\UserName;
+use App\Domain\Git\User\ValueObject\UserProjectId;
 
 final readonly class User
 {
     public function __construct(
-        private UserName $name,
-        private UserEmail $email,
+        public UserId $id,
+        public UserProjectId $projectId,
+        public UserName $name,
+        public UserEmail $email,
     ) {
-    }
-
-    public function getName(): UserName
-    {
-        return $this->name;
-    }
-
-    public function getEmail(): UserEmail
-    {
-        return $this->email;
     }
 }
