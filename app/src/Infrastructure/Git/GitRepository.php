8be44afbfb2c879d09dfb83920a689f70bf3fe64
Anton Ilin <ilin.antonio@gmail.com>
2024-09-20 00:55:01 +0500

    fix: empty value

--- app/src/Infrastructure/Git/GitRepository.php
+++ app/src/Infrastructure/Git/GitRepository.php
@@ -47,7 +47,7 @@ final readonly class GitRepository implements GitRepositoryInterface
         $log = shell_exec("cd {$project->path->getValue()} " .
             "&& git log --shortstat --no-merges --date=iso-local --since='{$since}'" .
             " --format='{|c|}{|p|}commit: %H{|p|}date: %aI{|p|}name: %aN{|p|}email: %aE{|p|}stat:'" .
-            " | tr '\n' ' '");
+            " | tr '\n' ' '") ?? '';
         $logItems = explode('{|c|}', $log);
 
         foreach ($logItems as $logItem) {
