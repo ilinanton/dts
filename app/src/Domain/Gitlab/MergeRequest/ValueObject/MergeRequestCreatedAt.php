2beb82ce536aa771b89bcc0ea1fa5ba845df3410
Anton Ilin <ilin.antonio@gmail.com>
2024-10-11 14:07:50 +0500

    refactor: remove dub code

--- app/src/Domain/Gitlab/MergeRequest/ValueObject/MergeRequestCreatedAt.php
+++ app/src/Domain/Gitlab/MergeRequest/ValueObject/MergeRequestCreatedAt.php
@@ -2,29 +2,8 @@
 
 namespace App\Domain\Gitlab\MergeRequest\ValueObject;
 
-use DateTimeImmutable;
-use InvalidArgumentException;
+use App\Domain\Common\ValueObject\AbstractDate;
 
-final class MergeRequestCreatedAt
+final readonly class MergeRequestCreatedAt extends AbstractDate
 {
-    private DateTimeImmutable $value;
-
-    public function __construct(string $value)
-    {
-        $this->assertValueIsValid($value);
-        $this->value = DateTimeImmutable::createFromFormat(DATE_RFC3339_EXTENDED, $value);
-    }
-
-    public function getValue(): string
-    {
-        return $this->value->format('Y-m-d H:i:s');
-    }
-
-    private function assertValueIsValid(string $value): void
-    {
-        $dateTime = DateTimeImmutable::createFromFormat(DATE_RFC3339_EXTENDED, $value);
-        if (false === $dateTime) {
-            throw new InvalidArgumentException('Created at is incorrect!');
-        }
-    }
 }
