19ea53ed8d81c53eaea6d60830ced1c7e6832cb0
Anton Ilin <ilin.antonio@gmail.com>
2025-05-11 18:35:08 +0500

    feat: save method

--- app/src/Infrastructure/Gitlab/GitlabMySqlResourceLabelEventRepository.php
+++ app/src/Infrastructure/Gitlab/GitlabMySqlResourceLabelEventRepository.php
@@ -18,16 +18,41 @@ final readonly class GitlabMySqlResourceLabelEventRepository implements GitlabDa
     public function save(ResourceLabelEvent $object): void
     {
         $sql = <<<SQL
-INSERT INTO gitlab_label (id, name, color)
-VALUES (:ID, :NAME, :COLOR)
-ON DUPLICATE KEY UPDATE name = :NAME, color = :COLOR
+INSERT INTO gitlab_resource_label_event
+    (
+     id,
+     user_id,
+     created_at,
+     resource_type,
+     resource_id,
+     label_id,
+     action_name,
+     project_id
+    )
+VALUES
+    (
+     :ID,
+     :USER_ID,
+     :CREATED_AT,
+     :RESOURCE_TYPE,
+     :RESOURCE_ID,
+     :LABEL_ID,
+     :ACTION_NAME,
+     :PROJECT_ID
+    )
+ON DUPLICATE KEY UPDATE id = id
 SQL;
 
         $stmt = $this->pdo->prepare($sql);
         $stmt->execute([
             ':ID' => $object->id->value,
-            ':NAME' => $object->name->value,
-            ':COLOR' => $object->color->value,
+            ':USER_ID' => $object->userId->value,
+            ':CREATED_AT' => $object->createdAt->getValue(),
+            ':RESOURCE_TYPE' => $object->resourceType->value,
+            ':RESOURCE_ID' => $object->resourceId->value,
+            ':LABEL_ID' => $object->labelId->value,
+            ':ACTION_NAME' => $object->actionName->value,
+            ':PROJECT_ID' => $object->projectId->value,
         ]);
     }
 }
