d93923694836700da4382bb8138a40f83d692ce7
Anton Ilin <ilin.antonio@gmail.com>
2025-09-28 14:04:27 +0500

    refactor: refactor

--- app/src/Application/Cli/ExitUseCase.php
+++ app/src/Application/Cli/ExitUseCase.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace App\Application\Cli;
 
 use App\Application\UseCaseInterface;
+use Exception;
 
 final readonly class ExitUseCase implements UseCaseInterface
 {
     public function execute(): void
     {
-        throw new \Exception('Goodbye!', 0);
+        throw new Exception('Goodbye!', 0);
     }
 }
